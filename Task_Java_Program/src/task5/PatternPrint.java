31e36e175ab29795afa00ed7a1ae6b05652564b9
nataraj-guvi <nataraj.guvi@gmail.com>
2025-05-07 23:43:04 +0530

    patterncommit

--- Task_Java_Program/src/task5/PatternPrint.java
+++ Task_Java_Program/src/task5/PatternPrint.java
@@ -4,21 +4,21 @@ import java.util.Scanner;
 
 public class PatternPrint {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		Scanner scan = new Scanner(System.in);
 		System.out.print("Enter the number of rows: ");
-		int rows = sc.nextInt();
+		int rows = scan.nextInt();
 		for (int i = 0; i < rows; i++) {
 			for (int j = 0; j <= rows; j++) {
 				if ((i == 0 && (j == 0 || j == rows - 1)) || (i == 1 && (j == 1 || j == rows - 2))
-						|| (i == 2 && j == rows / 2) || (i == 3 && (j == 0 || j == rows - 2))
-						|| (i == 4 && (j == 0 || j == rows))) {
+						|| (i == 2 && j == rows / 2) || (i == 3 && (j == 1 || j == rows - 2))
+						|| (i == 4 && (j == 1 || j == rows - 1))) {
 					System.out.print("*");
 				} else {
 					System.out.print(" ");
 				}
 			}
 			System.out.println();
+			scan.close();
 		}
-		sc.close();
 	}
 }
